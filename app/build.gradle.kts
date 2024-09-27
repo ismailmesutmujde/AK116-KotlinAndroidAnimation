0fbc790e031979110e86ce4100470fff3898f6ac
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-27 16:54:19 +0300

    updated: ui design and pre-coding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinandroidanimation"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinandroidanimation"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlinandroidanimation/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinandroidanimation/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.ismailmesutmujde.kotlinandroidanimation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinandroidanimation.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
+
+        bindingMain.buttonStart.setOnClickListener {
+            Toast.makeText(applicationContext, "TEST", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }

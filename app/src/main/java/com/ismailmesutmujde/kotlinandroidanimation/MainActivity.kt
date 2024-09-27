e4734cf6caa4f333bdaa4c29a5f69209b1561416
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-27 17:31:59 +0300

    updated: object animator - other properties

--- app/src/main/java/com/ismailmesutmujde/kotlinandroidanimation/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinandroidanimation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ismailmesutmujde.kotlinandroidanimation
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.animation.BounceInterpolator
+import android.view.animation.OvershootInterpolator
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ismailmesutmujde.kotlinandroidanimation.databinding.ActivityMainBinding
@@ -26,7 +27,10 @@ class MainActivity : AppCompatActivity() {
             //alphaAnimation()
             //scaleAnimation()
             //rotateAnimation()
-            translationAnimation()
+            //translationAnimation()
+            //delayFeature()
+            //accelerationFeature()
+            repeatFeature()
         }
 
     }
@@ -75,6 +79,33 @@ class MainActivity : AppCompatActivity() {
     fun imageFromLeftToRight() {
         val translation = ObjectAnimator.ofFloat(bindingMain.imageViewAppLogo, "translationX",-800.0f, 0.0f).apply {
             duration = 1000
+        }
+        translation.start()
+    }
+
+    fun delayFeature() {
+        val translation = ObjectAnimator.ofFloat(bindingMain.imageViewAppLogo, "translationY",0.0f, 50.0f).apply {
+            duration = 3000
+            startDelay = 2000
+        }
+        translation.start()
+    }
+
+    fun accelerationFeature() {
+        val translation = ObjectAnimator.ofFloat(bindingMain.imageViewAppLogo, "translationY",0.0f, 300.0f).apply {
+            duration = 1000
+            //interpolator = OvershootInterpolator()
+            interpolator = BounceInterpolator()
+        }
+        translation.start()
+    }
+
+    fun repeatFeature() {
+        val translation = ObjectAnimator.ofFloat(bindingMain.imageViewAppLogo, "translationY",0.0f, 100.0f).apply {
+            duration = 1000
+            repeatCount = ObjectAnimator.INFINITE
+            //repeatMode = ObjectAnimator.RESTART
+            repeatMode = ObjectAnimator.REVERSE
         }
         translation.start()
     }
